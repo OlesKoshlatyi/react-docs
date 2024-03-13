123cecb4c2721e722a18cc0a5c8a772e1303f727
Oles K. <lessenover@gmail.com>
2024-03-13 16:27:07 +0200

    Add Square component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from "react";
+import "./styles.css";
+
+const Square = () => {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue(x);
+  }
+
+  return (
+    <button className="square" onClick={handleClick}>
+      {value}
+    </button>
+  );
+};
 
 const App = () => {
-  return <div>Hello, react!!!</div>;
+  return (
+    <>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </>
+  );
 };
 
 export default App;
